fe4845095736af73a1966b7556caa26b5c2abf6c
ChanhuiSeok <chanhuicom@gmail.com>
2022-03-16 00:45:03 +0900

    project init(디펜던시 오류 수정)

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import Phaser, { Types } from "phaser";
+import logoImg from "./assets/logo.png";
+
+class MyGame extends Phaser.Scene {
+  constructor() {
+    super("demo");
+  }
+
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+  }
+}
+
+const config: Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyGame,
+};
+
+const game = new Phaser.Game(config);
